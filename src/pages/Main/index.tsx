50b004d1d87e192efca328f3babcb616877cf030
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-10 10:33:25 -0300

    Card

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,14 +4,39 @@ import { StatusBar, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Header } from "../../components/Header";
 import { Tabs } from "../../components/Tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export function Main() {
   return (
     <>
       <StatusBar barStyle={"light-content"} backgroundColor="#8A05BE" />
       <View style={styles.container}>
-        <Header/>
-        <Tabs/>
+        <Header />
+
+        <View style={styles.content}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <MaterialIcons name="attach-money" size={28} color="#665" />
+            
+              <MaterialIcons name="visibility-off" size={20} color="#665" />
+            </View>
+            
+            <View style={styles.cardContent}>
+              <Text style={styles.title}>Saldo disponível</Text>
+            
+              <Text style={styles.description}>R$ 597.644,50</Text>
+            </View>
+            
+            <View style={styles.cardFooter}>
+              <Text style={styles.annotation}>
+                Transferência de R$ 5.444,33 reccebida de Olívia Cristina hoje
+                às 08h00min.
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        <Tabs />
       </View>
     </>
   );
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -4,8 +4,53 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#8B10AE",
+    backgroundColor: "#8A05BE",
     width: "100%",
     paddingTop: getStatusBarHeight() + 17,
-  }
+    justifyContent: "center",
+  },
+  content: {
+    flex: 1,
+    maxHeight: 400,
+    zIndex: 5,
+  },
+  card: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 4,
+    marginTop: 0,
+    marginHorizontal: 20,
+    height: "100%",
+  },
+  cardHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 30
+  },
+  cardContent: {
+    flex: 1,
+    paddingTop: 0,
+    paddingHorizontal: 30,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 13,
+    color: "#998",
+  },
+  description: {
+    fontSize: 32,
+    marginTop: 3,
+    color: "#333",
+  },
+  cardFooter: {
+    padding: 30,
+    backgroundColor: "#EEE",
+    borderRadius: 4
+  },
+  annotation: {
+    fontSize: 12,
+    color: "#333",
+  },
+
 });
