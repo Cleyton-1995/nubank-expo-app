78fb837dc7855d1ac1430c944fe91f12c15e05b8
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-10 10:43:56 -0300

    depedency

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -5,6 +5,7 @@ import { styles } from "./styles";
 import { Header } from "../../components/Header";
 import { Tabs } from "../../components/Tabs";
 import { MaterialIcons } from "@expo/vector-icons";
+import { Menu } from "../../components/Menu";
 
 export function Main() {
   return (
@@ -14,19 +15,21 @@ export function Main() {
         <Header />
 
         <View style={styles.content}>
+          <Menu />
+
           <View style={styles.card}>
             <View style={styles.cardHeader}>
               <MaterialIcons name="attach-money" size={28} color="#665" />
-            
+
               <MaterialIcons name="visibility-off" size={20} color="#665" />
             </View>
-            
+
             <View style={styles.cardContent}>
               <Text style={styles.title}>Saldo disponível</Text>
-            
+
               <Text style={styles.description}>R$ 597.644,50</Text>
             </View>
-            
+
             <View style={styles.cardFooter}>
               <Text style={styles.annotation}>
                 Transferência de R$ 5.444,33 reccebida de Olívia Cristina hoje
