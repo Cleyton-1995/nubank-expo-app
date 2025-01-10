ee0f972844be13400cd7bd02e2d43532dbf65683
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-01-10 15:56:26 -0300

    Animations

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,46 +1,86 @@
 import React from "react";
-import { StatusBar, Text, View } from "react-native";
+import { Animated, StatusBar, Text, View } from "react-native";
 
 import { styles } from "./styles";
 import { Header } from "../../components/Header";
 import { Tabs } from "../../components/Tabs";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Menu } from "../../components/Menu";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
 
 export function Main() {
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event<PanGestureHandlerGestureEvent>(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandlerStateChanged(event: any) {}
+
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle={"light-content"} backgroundColor="#8A05BE" />
       <View style={styles.container}>
         <Header />
 
         <View style={styles.content}>
           <Menu/>
-          
-          <View style={styles.card}>
-            <View style={styles.cardHeader}>
-              <MaterialIcons name="attach-money" size={28} color="#665" />
 
-              <MaterialIcons name="visibility-off" size={20} color="#665" />
-            </View>
+          <PanGestureHandler
+            onGestureEvent={animatedEvent}
+            onHandlerStateChange={onHandlerStateChanged}
+          >
+            <Animated.View
+              style={[
+                styles.card,
+                {
+                  transform: [
+                    {
+                      translateY: translateY.interpolate({
+                        inputRange: [-350, 0, 380],
+                        outputRange: [-50, 0, 380],
+                        extrapolate: "clamp",
+                      }),
+                    },
+                  ],
+                },
+              ]}
+            >
+              <View style={styles.cardHeader}>
+                <MaterialIcons name="attach-money" size={28} color="#665" />
 
-            <View style={styles.cardContent}>
-              <Text style={styles.title}>Saldo disponível</Text>
+                <MaterialIcons name="visibility-off" size={20} color="#665" />
+              </View>
 
-              <Text style={styles.description}>R$ 597.644,50</Text>
-            </View>
+              <View style={styles.cardContent}>
+                <Text style={styles.title}>Saldo disponível</Text>
 
-            <View style={styles.cardFooter}>
-              <Text style={styles.annotation}>
-                Transferência de R$ 5.444,33 reccebida de Olívia Cristina hoje
-                às 08h00min.
-              </Text>
-            </View>
-          </View>
+                <Text style={styles.description}>R$ 597.644,50</Text>
+              </View>
+
+              <View style={styles.cardFooter}>
+                <Text style={styles.annotation}>
+                  Transferência de R$ 5.444,33 reccebida de Olívia Cristina hoje
+                  às 08h00min.
+                </Text>
+              </View>
+            </Animated.View>
+          </PanGestureHandler>
         </View>
 
         <Tabs />
       </View>
-    </>
+    </GestureHandlerRootView>
   );
 }
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -24,7 +24,7 @@ export const styles = StyleSheet.create({
     position: "absolute",
     left: 0,
     right: 0,
-    top: 380,
+    top: 0,
   },
   cardHeader: {
     flexDirection: "row",
